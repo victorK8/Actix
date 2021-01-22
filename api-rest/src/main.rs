7a818d73f1903af8cd0cbdd2450da26664bec11b
victorK8 <victormalum@gmail.com>
2021-01-22 13:51:23 +0100

    firts version of api

--- api-rest/src/main.rs
+++ api-rest/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+/// Api-Rest example 
+/// by Víctor Malumbres Talles
+/// Ubuntu 20.04 LTS Machine
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hey sunshine!")
+}#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hi", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
